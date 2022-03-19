7765e3f700246325e48164bd9bb4d70e7970b02d
Jim Briggs <jbriggs462@gmail.com>
2022-03-19 18:14:13 -0400

    implement computerPlay func and test

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,21 @@
-// This game will be played thru the console
+/* This game will be played thru the console
+take random number. If num <= 0.33 = Rock, num >= 0.66 = Scissors,
+else Paper */
+function computerPlay() {
+    let comp = Math.random();
+    
+    if(comp <= 0.33) {
+        return "Rock";
+    }
+    else if(comp >= 0.66) {
+        return "Scissors";
+    }
+   
+    return "Paper";
+}
+
+/* play a single round. compare computer and player and determine winner
+rock > scissors, paper > rock, scissors > paper */
+function playRound(playerSelection, computerSelection) {
+
+}
