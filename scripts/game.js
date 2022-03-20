f69bab4cc5fa0a431866b1b79e6a7e1765d38c39
Jim Briggs <jbriggs462@gmail.com>
2022-03-19 21:00:09 -0400

    started playRound function

--- scripts/game.js
+++ scripts/game.js
@@ -17,5 +17,17 @@ function computerPlay() {
 /* play a single round. compare computer and player and determine winner
 rock > scissors, paper > rock, scissors > paper */
 function playRound(playerSelection, computerSelection) {
+    let winner = "Tie";
+
+    if(playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
+        winner = "Player";
+    }
+    else if(playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
+        winner = "Player";
+    }
+    else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
+        winner = "Player";
+    }
+
 
 }
