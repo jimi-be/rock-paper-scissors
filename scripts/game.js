67d7baf912c63f271ce61dfc594d3da7d66b2c16
Jim Briggs <jbriggs462@gmail.com>
2022-03-20 21:18:00 -0400

    finished game function and updated html

--- scripts/game.js
+++ scripts/game.js
@@ -1,17 +1,21 @@
-/* This game will be played thru the console
-take random number. If num <= 0.33 = Rock, num >= 0.66 = Scissors,
-else Paper */
+/* global variables to keep score.
+I don't like this method, but it's all I know at this point*/
+let playerScore = 0;
+let compScore = 0;
+
+/* Generate computer selection based on random number
+If random num <= 0.33 = Rock, random num >= 0.66 = Scissors, else Paper */
 function computerPlay() {
     let comp = Math.random();
     
     if(comp <= 0.33) {
-        return "rock";
+        return "Rock";
     }
     else if(comp >= 0.66) {
-        return "scissors";
+        return "Scissors";
     }
    
-    return "paper";
+    return "Paper";
 }
 
 // format text to lowercase with capital 1st letter
@@ -33,26 +37,46 @@ function playRound(playerSelection, computerSelection) {
 
     if(player == "Rock") {
         if (computerSelection == "Scissors") {
+            playerScore++;
             return `You Win! Computer: ${computerSelection}, You: ${player}`;
         }
         else {
+            compScore++;
             return `You Lose! Computer: ${computerSelection}, You: ${player}`;
         }
     }
     if(player == "Paper") {
         if (computerSelection == "Rock") {
+            playerScore++;
             return `You Win! Computer: ${computerSelection}, You: ${player}`;
         }
         else {
+            compScore++;
             return `You Lose! Computer: ${computerSelection}, You: ${player}`;
         }
     }
     if(player == "Scissors") {
         if (computerSelection == "Paper") {
+            playerScore++;
             return `You Win! Computer: ${computerSelection}, You: ${player}`;
         }
         else {
+            compScore++;
             return `You Lose! Computer: ${computerSelection}, You: ${player}`;
         }
     }
+
+    return `ERROR: ${player} is not a valid selection.`
+}
+
+/* This game will be played thru the console for now */
+function game() {
+    for (let i = 0; i< 5; i++) {
+        const playerSelection = prompt("Your selection: Rock, Paper, or Scissors?");
+        const computerSelection = computerPlay();
+
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    console.log(`Final score - You: ${playerScore}, Computer: ${compScore}`);
 }
