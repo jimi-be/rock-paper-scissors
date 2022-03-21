cae5dcceee4ecd4755a50d5530ba1a9f6a39fdcd
Jim Briggs <jbriggs462@gmail.com>
2022-03-20 20:12:01 -0400

    finished playRound function

--- scripts/game.js
+++ scripts/game.js
@@ -5,29 +5,54 @@ function computerPlay() {
     let comp = Math.random();
     
     if(comp <= 0.33) {
-        return "Rock";
+        return "rock";
     }
     else if(comp >= 0.66) {
-        return "Scissors";
+        return "scissors";
     }
    
-    return "Paper";
+    return "paper";
+}
+
+// format text to lowercase with capital 1st letter
+function formatText(text) {
+    let l1=text.toLowerCase();
+    let cap=l1.charAt(0).toUpperCase();
+    let part=l1.slice(1);
+    return cap.concat(part);
 }
 
 /* play a single round. compare computer and player and determine winner
 rock > scissors, paper > rock, scissors > paper */
 function playRound(playerSelection, computerSelection) {
-    let winner = "Tie";
+    let player = formatText(playerSelection);
 
-    if(playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
-        winner = "Player";
-    }
-    else if(playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
-        winner = "Player";
-    }
-    else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
-        winner = "Player";
+    if(player === computerSelection) {
+        return `Tie, both players chose ${player}`;
     }
 
-
+    if(player == "Rock") {
+        if (computerSelection == "Scissors") {
+            return `You Win! Computer: ${computerSelection}, You: ${player}`;
+        }
+        else {
+            return `You Lose! Computer: ${computerSelection}, You: ${player}`;
+        }
+    }
+    if(player == "Paper") {
+        if (computerSelection == "Rock") {
+            return `You Win! Computer: ${computerSelection}, You: ${player}`;
+        }
+        else {
+            return `You Lose! Computer: ${computerSelection}, You: ${player}`;
+        }
+    }
+    if(player == "Scissors") {
+        if (computerSelection == "Paper") {
+            return `You Win! Computer: ${computerSelection}, You: ${player}`;
+        }
+        else {
+            return `You Lose! Computer: ${computerSelection}, You: ${player}`;
+        }
+    }
 }
